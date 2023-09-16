376fa05e093164bf4b7244b421917bb3b65a5d22
promptdrake <95160973+promptdrake@users.noreply.github.com>
2023-09-16 15:12:56 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    "bot_token": "",
-    "target_forward": "" // Replace your chatid check using our telegram bot https://t.me/mindpastebot
+    "bot_token": "5823735240:AAErus-OZMEAEpNihLmQU4RV9Ys2Y39e584",
+    "target_forward": "6169423396", // Replace your chatid check using our telegram bot https://t.me/mindpastebot
+    "port": "80" // Used For Webhook api handler, if you have contact form
 }
